362a45d5e8d7f3dbecd21c4bdd3261b479ff30fe
absolutenaman <absolute.naman@gmail.com>
2025-11-08 19:57:04 +0530

    Optimised code for partition in linked list

--- Medium/81 Partition.java
+++ Medium/81 Partition.java
@@ -1,22 +1,33 @@
-  public class ListNode {
-     int val;
-     ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- }
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+ // Nice question to be honest
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        ListNode curr = head;
-        ListNode newHead = new ListNode();
-        ListNode newCurr = newHead;
-        while (curr != null) {
-            if (curr.val < x) {
-                newCurr.val = curr.val;
-                newCurr.next = new ListNode();
+        ListNode leftNode=new ListNode();
+        ListNode currLeftNode=leftNode;
+        ListNode rightNode=new ListNode();
+        ListNode currRightNode=rightNode;
+        ListNode curr=head;
+        while(curr!=null){
+            if(curr.val < x) {
+                currLeftNode.next=curr;
+                currLeftNode=curr;
+            } else {
+                currRightNode.next=curr;
+                currRightNode=curr;
             }
-            curr = curr.next;
+            curr=curr.next;
         }
-        return newHead;
+                currRightNode.next=null;
+        currLeftNode.next=rightNode.next;
+        return leftNode.next;
     }
 }
